2a6a095f307a87f48e0afbd09b29d11f8063e2a9
maki1337 <matevz.mak@gmail.com>
2019-06-14 16:05:36 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -143,7 +143,7 @@ textForm.addEventListener('submit', e => {
 
 function fetchTrafficData() {
     console.log("Fetching traffic inforamtion");
-    fetch('https://opendata.si/promet/events/?lang=en')
+    fetch('https://opendata.si/promet/events/')
         .then(function (response) {
             return response.json();
         })
